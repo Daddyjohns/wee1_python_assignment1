ba7c8660df6fb4e1e75c861a01356173da0edd75
Owino John <johnsojowino5@gmail.com>
2025-07-29 22:10:18 +0300

    my first python assignment

---
+++ intro.py
@@ -0,0 +1,10 @@
+first_number = int(input("enter a first number"))
+second_number = int(input("enter the second number"))
+sum_of_numbers = first_number + second_number
+product_of_numbers = first_number * second_number
+difference_of_numbers = first_number - second_number
+division_of_numbers = first_number/second_number
+print("The sum is",sum_of_numbers)
+print("The product is",product_of_numbers)
+print("The difference is",difference_of_numbers)
+print("The division is",division_of_numbers)
